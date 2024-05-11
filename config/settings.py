81e9daf660e87df0c250dd867dfbdc96ea72c19a
Bobur-inoyatovv <164325696+Bobur-inoyatovv@users.noreply.github.com>
2024-05-11 17:42:52 +0500

    OK

--- config/settings.py
+++ config/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-STATICFILES_DIRS = [BASE_DIR / 'blog/static']
+STATICFILES_DIRS = [BASE_DIR]
 
 MEDIA_URL = '/media/'  # new
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # new
